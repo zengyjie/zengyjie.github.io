e5932a7380744ad2b98e30fb3fa8f386a44fb66c
zengyjie <technic.mechanic77@gmail.com>
2021-12-20 18:12:51 +0800

    test

--- script2.js
+++ script2.js
@@ -1,15 +1,18 @@
-var r
-var g
-var b
+var r = 0
+var g = 0
+var b = 0
 
 function changeR() {
     r += 1
+    document.getElementById('colorDisplay').innerHTML = r
 }
 
 function changeG() {
     g += 1
+    document.getElementById('colorDisplay').innerHTML = g
 }
 
 function changeB() {
     b += 1
+    document.getElementById('colorDisplay').innerHTML = b
 }
