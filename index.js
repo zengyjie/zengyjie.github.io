90b63caaf0a662cd909b80bafa742cd5b2082c50
zengyjie <technic.mechanic77@gmail.com>
2021-12-22 18:07:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,8 @@ class Player {
     }
 }
 
-const player = new Player(100, 100, 30, 'green')
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const player = new Player(x, y, 30, 'green')
 player.draw()
